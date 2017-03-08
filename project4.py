7c03fee47476e3f098476788c53a2142fef0bbe6
arvindmvepa <arvind.m.vepa@gmail.com>
2017-03-07 23:24:08 -0800

    another update to 5 and 6

--- project4.py
+++ project4.py
@@ -50,7 +50,6 @@ def determineClasses(x):
         return int(4)
     else:
         return int(5)
-
 # PART 1:
 # parse text data and convert to tfidf representation
 comp_categories = ['comp.graphics','comp.os.ms-windows.misc','comp.sys.ibm.pc.hardware','comp.sys.mac.hardware']
@@ -199,56 +198,64 @@ tfidf_transformer = TfidfTransformer()
 groups = fetch_20newsgroups(subset='all', shuffle=True, random_state=42)
 counts = vectorizer.fit_transform(groups.data)
 X = tfidf_transformer.fit_transform(counts)
-
-u, s, vt = svds(X, k=20, which='LM', return_singular_vectors=True)
-
+X = preprocessing.normalize(X, norm='l2')
 dimensions = range(2,20)
 results_svd = []
 results_nmf = []
-
-targets = map(lambda x: int(x) , groups.target.tolist())
+#targets = map(lambda x: int(x) , groups.target.tolist())
+targets = groups.target
 
 # sweeping over the dimension parameter
 for dimension in dimensions:
-
+    
     svd = TruncatedSVD(n_components=dimension, random_state=42)
     X_reduced_svd = svd.fit_transform(X)
-
-    nmf = NMF(n_components=dimension)
-    X_reduced_nmf = nmf.fit_transform(X)
-
-    predictions_svd = KMeans(n_clusters=20, max_iter=100).fit_predict(X_reduced_svd)
+    X_reduced_svd[X_reduced_svd <= 0] = 1e-5
+    X_reduced_svd = np.log(X_reduced_svd)
+    predictions_svd = KMeans(n_clusters=20, max_iter=100, init='k-means++')
+    predictions_svd.fit(X_reduced_svd)
+    
     print "Part 5 ..."
     print "SVD Dimension Reduction with k = %d" %(dimension)
     print "Confusion Matrix:"
-    print(confusion_matrix(targets, predictions_svd))
-    print "Homogeneity Score: " + str(homogeneity_score(targets, predictions_svd))
-    print "Completeness Score: " + str(completeness_score(targets, predictions_svd))
-    print "Adjusted Rand Score: " + str(adjusted_rand_score(targets, predictions_svd))
-    print "Adjusted Mutual Info Score: " + str(adjusted_mutual_info_score(targets, predictions_svd))
+    print(confusion_matrix(targets, predictions_svd.labels_))
+    print "Homogeneity Score: " + str(homogeneity_score(targets, predictions_svd.labels_))
+    print "Completeness Score: " + str(completeness_score(targets, predictions_svd.labels_))
+    print "Adjusted Rand Score: " + str(adjusted_rand_score(targets, predictions_svd.labels_))
+    print "Adjusted Mutual Info Score: " + str(adjusted_mutual_info_score(targets, predictions_svd.labels_))
 
-    results_svd.append(homogeneity_score(targets, predictions_svd))
+    results_svd.append(homogeneity_score(targets, predictions_svd.labels_))
+    
+    nmf = NMF(n_components=dimension)
+    X_reduced_nmf = nmf.fit_transform(X)
+    X_reduced_nmf[X_reduced_nmf <= 0] = 1e-5
+    X_reduced_nmf = np.log(X_reduced_nmf)
+    predictions_nmf = KMeans(n_clusters=20, max_iter=100, init='k-means++')
+    predictions_nmf.fit(X_reduced_nmf)
 
-    predictions_nmf = KMeans(n_clusters=20, max_iter=100).fit_predict(X_reduced_nmf)
     print "Part 5 ..."
     print "NMF Dimension Reduction with k = %d" %(dimension)
     print "Confusion Matrix:"
-    print(confusion_matrix(targets, predictions_nmf))
-    print "Homogeneity Score: " + str(homogeneity_score(targets, predictions_nmf))
-    print "Completeness Score: " + str(completeness_score(targets, predictions_nmf))
-    print "Adjusted Rand Score: " + str(adjusted_rand_score(targets, predictions_nmf))
-    print "Adjusted Mutual Info Score: " + str(adjusted_mutual_info_score(targets, predictions_nmf))
+    print(confusion_matrix(targets, predictions_nmf.labels_))
+    print "Homogeneity Score: " + str(homogeneity_score(targets, predictions_nmf.labels_))
+    print "Completeness Score: " + str(completeness_score(targets, predictions_nmf.labels_))
+    print "Adjusted Rand Score: " + str(adjusted_rand_score(targets, predictions_nmf.labels_))
+    print "Adjusted Mutual Info Score: " + str(adjusted_mutual_info_score(targets, predictions_nmf.labels_))
 
-    results_nmf.append(homogeneity_score(targets, predictions_nmf))
-
+    results_nmf.append(homogeneity_score(targets, predictions_nmf.labels_))
+                       
 # Best Dimension
 print "Best Dimension for SVD: " + str(dimensions[results_svd.index(max(results_svd))])
 print "Best Dimension for NMF: " + str(dimensions[results_nmf.index(max(results_nmf))])
 
 # With normalization and nonlinearity, and best dimensions for SVD and NMF
+
+maxSVD = dimensions[results_svd.index(max(results_svd))]
+maxNMF = dimensions[results_nmf.index(max(results_nmf))]
+
 X_normalized = preprocessing.normalize(X, norm='l2')
 
-svd = TruncatedSVD(n_components=5, random_state=42)
+svd = TruncatedSVD(n_components=maxSVD, random_state=42)
 X_reduced_svd = svd.fit_transform(X_normalized)
 X_reduced_svd[X_reduced_svd <= 0] = 1e-5
 X_norm_nonlinear_svd = np.log(X_reduced_svd)
@@ -263,7 +270,7 @@ print "Completeness Score: " + str(completeness_score(targets, predictions_norm_
 print "Adjusted Rand Score: " + str(adjusted_rand_score(targets, predictions_norm_nonlin_svd))
 print "Adjusted Mutual Info Score: " + str(adjusted_mutual_info_score(targets, predictions_norm_nonlin_svd))
 
-nmf = NMF(n_components=5)
+nmf = NMF(n_components=maxNMF)
 X_reduced_nmf = nmf.fit_transform(X_normalized)
 X_reduced_nmf[X_reduced_nmf <= 0] = 1e-5
 X_norm_nonlinear_nmf = np.log(X_reduced_nmf)
@@ -286,6 +293,7 @@ tfidf_transformer = TfidfTransformer()
 groups = fetch_20newsgroups(subset='all',shuffle=True, random_state=42)
 counts = vectorizer.fit_transform(groups.data)
 X = tfidf_transformer.fit_transform(counts)
+X = preprocessing.normalize(X, norm='l2')
 
 u, s, vt = svds(X, k=10, which='LM', return_singular_vectors=True)
 
@@ -302,9 +310,13 @@ for dimension in dimensions:
 
     svd = TruncatedSVD(n_components=dimension, random_state=42)
     X_reduced_svd = svd.fit_transform(X)
+    X_reduced_svd[X_reduced_svd <= 0] = 1e-5
+    X_reduced_svd = np.log(X_reduced_svd)
 
     nmf = NMF(n_components=dimension)
     X_reduced_nmf = nmf.fit_transform(X)
+    X_reduced_nmf[X_reduced_nmf <= 0] = 1e-5
+    X_reduced_nmf = np.log(X_reduced_nmf)
 
     predictions_svd = KMeans(n_clusters=6, max_iter=100).fit_predict(X_reduced_svd)
     print "Part 5 ..."
